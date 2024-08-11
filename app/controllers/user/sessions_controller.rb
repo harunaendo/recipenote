fd3da5df176d42ee05402e0bc68335505a753f96
harunaendo <163505612+harunaendo@users.noreply.github.com>
2024-08-11 13:46:41 +0000

    finish

--- app/controllers/user/sessions_controller.rb
+++ app/controllers/user/sessions_controller.rb
@@ -1,12 +1,13 @@
 # frozen_string_literal: true
 
 class User::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
+   #before_action :configure_sign_in_params, only: [:create]
+   before_action :user_state, only: [:create]
 
   # GET /resource/sign_in
-  # def new
-  #   super
-  # end
+   def create
+     super
+   end
 
   # POST /resource/sign_in
   # def create
@@ -18,7 +19,27 @@ class User::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  # protected
+  protected
+
+  def user_state
+    @user = User.find_by(email: params[:user][:email]) #入力されたemailからアカウントを1件取得
+    return if @user.nil? #アカウントを取得できなかった場合、このメソッドを終了
+    return unless @user.valid_password?(params[:user][:password]) #取得したアカウントのパスワードと入力されたパスワードが一致していない場合、このメソッドを終了
+    if @user.is_active == false
+      flash[:alert] = "退会済みです。再度、新規登録をお願いいたします。"
+      redirect_to new_user_registration_path
+    end
+  end
+
+
+  def after_sign_in_path_for(resource)
+     user_root_path
+  end
+
+  #def after_sign_out_path_for(resource)
+    #アバウトページへリンク追加
+  #end
+
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   namespace :user do
-    get 'homes/top'
+    get 'recipes/new'
+    get 'recipes/index'
+    get 'recipes/edit'
+    get 'recipes/show'
   end
   # 顧客用
   # URL /users/sign_in ...
@@ -14,5 +17,11 @@ Rails.application.routes.draw do
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
     sessions: "admin/sessions"
   }
+
+  namespace :user do
+    root to: 'homes#top'
+    get '/about', to: 'homes#about'
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
