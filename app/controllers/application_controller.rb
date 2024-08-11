73ddca76b0fc36e734cc44c8c8e32d26c35f2d78
harunaendo <163505612+harunaendo@users.noreply.github.com>
2024-08-11 10:28:49 +0000

    save

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+
+# private
+
+#  def set_header
+#    if admin_signed_in?
+#      @header = 'admin'
+#    elsif user_signed_in?
+#      @header = 'user'
+#    else
+#      @header = 'guest'
+#    end
+#  end
+
 end
