90a2072e0089797cd31f306da9dda25bdfe17a9c
harunaendo <163505612+harunaendo@users.noreply.github.com>
2024-08-26 11:12:43 +0000

    [Remove] web_console

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -71,7 +71,7 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.web_console.whitelisted_ips = '118.83.252.187'
+  # config.web_console.whitelisted_ips = '118.83.252.187'
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
